02ac0c7195ad3ffc4ac4c15ef10eebfa1788556e
Guy D <guyd@satellogic.com>
2017-04-23 16:16:49 +0300

    migrating service to python and working with django-storages

--- README.md
+++ README.md
@@ -1,13 +1,40 @@
-# s3zipper
-Microservice that Servers Streaming Zip file from S3 Securely
+# zipper
+Microservice that Servers Streaming Zip file from a storgae
+
 
 ## Read the blog here
 [Original Blog Post](http://engineroom.teamwork.com/how-to-securely-provide-a-zip-download-of-a-s3-file-bundle/)
 
+## features:
+- uses django-storages so can be backend with any storage supported by this library, the current implementation is s3
+- writes a content of file using string directly without using a storage 
+- dynamically sets the name of the file
+
+## Known issues
+- streaming works between files and not in a file, which means that the service first download the file from s3 and only later streams it
+
+
+
+## schema:
+  ```
+  {
+ "zip_filename": <filename>,
+ "files":  
+  [{"content":<the content of a file>,"filename":<filename>},
+   {"path":"<path of the file using django storages>","filename":"filename"}
+  ]
+ }
+```
+
+## public docker images:
+- https://hub.docker.com/r/guydou/zipper-python/
+- https://hub.docker.com/r/guydou/zipper-rest/
+
+
 ## Docker-compose
 running:
 ```
-AWS_S3_BUCKET_NAME=<bucket> AWS_REGION=<region> AWS_ACCESS_KEY_ID=<key> AWS_SECRET_ACCESS_KEY=<secret> docker-compose  -f docker-compose.yml -f docker-compose.development.yml up
+AWS_STORAGE_BUCKET_NAME=<bucket> AWS_REGION=<region> AWS_ACCESS_KEY_ID=<key> AWS_SECRET_ACCESS_KEY=<secret> docker-compose  -f docker-compose.yml  up
 ```
 
 you will have zipper server running on port 8123
---
+++ python-zipper/zipper/zipper/views.py
@@ -0,0 +1,39 @@
+from django.http import StreamingHttpResponse, HttpResponseBadRequest
+from zipstream import ZIP_DEFLATED, ZipFile
+from django.core.files.storage import default_storage
+import redis
+import json
+
+
+redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
+
+def read_in_chunks(file_object, chunk_size=1024):
+    """Lazy function (generator) to read a file piece by piece.
+    Default chunk size: 1k."""
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            file_object.close()
+            break
+        yield data
+
+
+def zipball(request, token):
+    zip_content = redis_client.get("zip:%s" % (token))
+    if not(zip_content):
+        return HttpResponseBadRequest()
+    zip_content = json.loads(zip_content.decode('utf-8'))
+    zip_filename = zip_content['zip_filename']
+    z = ZipFile(mode='w', compression=ZIP_DEFLATED)
+    for file_def in zip_content['files']:
+        filepath = file_def.get("path", None)
+        file_content = file_def.get("content", None)
+        filename = file_def.get("filename", None)
+        if filepath:
+            f = default_storage.open(filepath, 'r')
+            z.write_iter(filename, read_in_chunks(f))
+        elif file_content:
+            z.writestr(filename, file_content.encode("utf-8"))
+    response = StreamingHttpResponse(z, content_type='application/zip')
+    response['Content-Disposition'] = 'attachment; filename={}'.format(zip_filename)
+    return response
